bbdf1e57ed5b2dcba621defbe0ee365cef6dbde5
Adit3107 <adityakalmegh01@gmail.com>
2026-09-17 14:18:27 +0530

    chore(backend): initialize go backend

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,7 @@
+package main // This is the main package for the backend server application. purpose - It serves as the entry point for the application, initializing and starting the server. (code builds into exe prog)
+
+import "fmt" // Importing the fmt package for formatted I/O operations.
+
+func main(){
+	fmt.Println("Memora backend Starting...")
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module memora-backend
+
+go 1.27.1
